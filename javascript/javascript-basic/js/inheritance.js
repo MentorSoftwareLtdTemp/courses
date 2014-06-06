30cc0dba92a9e2bd166acee57f0205e64931db86
Miroslaw Dylag <miroslaw.dylag@mentorsoftwareltd.com>
2014-06-06 14:46:11 +0200

    Delete old files. Add javascript basic.

--- javascript/javascript-basic/js/inheritance.js
+++ javascript/javascript-basic/js/inheritance.js
@@ -30,24 +30,3 @@ rect.move(1, 1); // Outputs, "Shape moved."
 
 //Object.create method 2
 
-function Shape() {
-    this.x = 0;
-    this.y = 0;
-}
-
-//Object.create
-function Shape() {
-    this.x = 0;
-    this.y = 0;
-}
-
-// superclass method
-Shape.prototype.move = function(x, y) {
-    this.x += x;
-    this.y += y;
-    console.info("Shape moved.");
-};
-
-var rect = Object.create(new Shape());
-console.log(rect instanceof Shape); // true.
-rect.move(1,1);
